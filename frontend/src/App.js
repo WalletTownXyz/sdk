47fe2b5a4eebfc76e26e9bdab66b7da51eee01b3
WalletTown <senateran590@gmail.com>
2026-03-18 18:30:58 +0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Wallet from "./components/Wallet";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>🌆 WalletTown</h1>
+      <Wallet />
+    </div>
+  );
+}
+
+export default App;
